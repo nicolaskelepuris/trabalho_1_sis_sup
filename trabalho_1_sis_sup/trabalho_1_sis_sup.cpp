30acf9a6d81b0b8b1eaeea237c7c2aa4e2b08fb1
nicolas <nicolas.kelepuris@gmail.com>
2021-09-24 22:57:19 -0300

    add usos da classe na main

--- trabalho_1_sis_sup/trabalho_1_sis_sup.cpp
+++ trabalho_1_sis_sup/trabalho_1_sis_sup.cpp
@@ -5,7 +5,21 @@ using namespace trabalho_1;
 
 int main()
 {
-    Investimento investimento1(1000.0, 0.1, 10);
-    Investimento investimento2(1000.0, 0.2, 0);
-    std::cout << investimento2.GetMontanteFinalPeriodoJurosSimples();
+    Investimento investimento1(1000.0, 0.1);
+    std::cout << investimento1.GetTaxaJurosMes();
+
+    std::cout << '\n';
+
+    Investimento investimento2(1000.0, 0.1, 10);
+    Investimento investimento3(1000.0, 0.15, 10);
+    bool same1 = investimento3.GetMontanteFinalPeriodoJurosCompostos() == Investimento::FindInvestimentoMaiorMontanteFinal(investimento2, investimento3).GetMontanteFinalPeriodoJurosCompostos();
+    std::cout << same1;
+
+    std::cout << '\n';
+
+
+    Investimento investimento4(1000.0, 0.1, 10);
+    Investimento investimento5(1000.0, 0.1, 0);
+    bool same2 = investimento4.GetMontanteFinalPeriodoJurosCompostos() == Investimento::FindInvestimentoMaiorMontanteFinal(investimento4, investimento5).GetMontanteFinalPeriodoJurosCompostos();
+    std::cout << same2;
 }
