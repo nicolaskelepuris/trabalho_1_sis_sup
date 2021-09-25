a503726ad77443068d61808f77aad6d2eba7a5c5
nicolas <nicolas.kelepuris@gmail.com>
2021-09-24 22:31:41 -0300

    add testes juros compostos

--- testes_trabalho_1_sis_sup/testes_trabalho_1_sis_sup.cpp
+++ testes_trabalho_1_sis_sup/testes_trabalho_1_sis_sup.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 #include "../trabalho_1_sis_sup/Investimento.h"
 #include "../trabalho_1_sis_sup/Investimento.cpp"
+#include <cmath>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace trabalho_1;
@@ -19,6 +20,21 @@ namespace testestrabalho1sissup
 
 			Investimento investimento2(1000.0, 0.1, 0);
 			Assert::AreEqual(1000.0, investimento2.GetMontanteFinalPeriodoJurosSimples());
+
+			Investimento investimento3(1000.0, 0.0, 10);
+			Assert::AreEqual(1000.0, investimento3.GetMontanteFinalPeriodoJurosSimples());
+		}
+
+		TEST_METHOD(testeMontanteFinalJurosCompostos)
+		{
+			Investimento investimento1(1000.0, 0.1, 10);
+			Assert::AreEqual(2594.0, round(investimento1.GetMontanteFinalPeriodoJurosCompostos()));
+
+			Investimento investimento2(1000.0, 0.1, 0);
+			Assert::AreEqual(1000.0, investimento2.GetMontanteFinalPeriodoJurosCompostos());
+
+			Investimento investimento3(1000.0, 0.0, 10);
+			Assert::AreEqual(1000.0, investimento3.GetMontanteFinalPeriodoJurosCompostos());
 		}
 	};
 }
